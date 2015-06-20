5c70475bec603fc80088ea230e9250d21f6c5e94
jgabry <jgabry@gmail.com>
2015-06-20 00:58:14 -0600

    package doc

---
+++ R/RHhelpers.R
@@ -0,0 +1,8 @@
+#' Convenience functions for working with CPRC/RH survey data
+#' @name RHhelpers
+#' @docType package
+#' @import survey
+#' @importFrom plyr ddply summarise mapvalues arrange mutate
+#' @importFrom parallel parLapply
+#' @importFrom mice mice ibind
+NULL
